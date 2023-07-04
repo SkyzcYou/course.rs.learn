ae59e5bc88fe30f298475d4c54d51af49ad46820
SkyzcYou <youzhengcai@gmail.com>
2023-07-04 23:58:19 +0800

    :sparkles: 2.0,2.1

---
+++ chapter2/variables/target/debug/deps/variables-b992fa5549e05d1e.d
@@ -0,0 +1,5 @@
+/home/skyzcyou/ProjectWarehouse/RustProjects/course.rs.learn/chapter2/variables/target/debug/deps/variables-b992fa5549e05d1e: src/main.rs
+
+/home/skyzcyou/ProjectWarehouse/RustProjects/course.rs.learn/chapter2/variables/target/debug/deps/variables-b992fa5549e05d1e.d: src/main.rs
+
+src/main.rs:
---
+++ chapter2/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/skyzcyou/ProjectWarehouse/RustProjects/course.rs.learn/chapter2/variables/target/debug/variables: /home/skyzcyou/ProjectWarehouse/RustProjects/course.rs.learn/chapter2/variables/src/main.rs
