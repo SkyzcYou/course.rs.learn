faf8347eb005f71b5c2c032701ad692846ad7047
SkyzcYou <youzhengcai@gmail.com>
2023-07-05 09:39:47 +0800

    Update main.rs

--- chapter2/2.1-variables/variables/src/main.rs
+++ chapter2/2.1-variables/variables/src/main.rs
@@ -1,6 +1,30 @@
 fn main() {
     let mut x = 5;
-    println!("the value of x is: {}",x);
+    println!("The value of x is: {}", x);
     x = 6;
-    println!("the value of x is: {}",x);
+    println!("The value of x is: {}", x);
+
+    // 变量解构：从一个相对复杂的变量中，匹配出该变量的一部分内容
+    // 和 Python Go 中的一个意思
+    let (a,mut b):(bool, bool) = (true, false);
+    println!("a = {:?}, b = {:?}", a, b);
+    b = true;
+    assert_eq!(a,b);
+
+}
+
+fn out(){
+    // 1.59 版本后，我们可以在赋值语句的左式中使用元组、切片和结构体模式了。
+    struct Struct{
+        e: i32
+    }
+    let (a,b,c,d,e);
+
+    (a,b) = (1,2);
+
+    [c,.., d,_] = [1,2,3,4,5];
+    Struct{e,..} = Struct{e:5};
+    assert_eq!([1,2,1,4,5], [a,b,c,d,e]);
+    // 这里用到了模式匹配的一些语法，如果大家看不懂没关系，可以在学完模式匹配章节后，再回头来看。
+
 }
